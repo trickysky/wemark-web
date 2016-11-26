e6a1cf65d42b3577dc79be8e5158a4ad2c0573fe
tiankun <iam@tiankun.me>
2016-11-26 16:07:24 +0800

    add datetimepicker

---
+++ static/managesite/managesite.js
@@ -0,0 +1,15 @@
+/**
+ * Created by tk on 2016/11/26.
+ */
+
+$("#newOrderModal").modal('show');
+$("#datePicker").datetimepicker({
+    format: "yyyy年mm月dd日",
+    showMeridian: true,
+    todayBtn: true,
+    language: "zh-CN",
+    startDate: (new Date()).toISOString().substring(0, 10),
+    autoclose: true,
+    minView: "month",
+    todayHighlight: true
+});
