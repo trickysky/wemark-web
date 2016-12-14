527562624d3246d0e99048db614bec2a6b97620b
tiankun <iam@tiankun.me>
2016-12-14 17:07:22 +0800

    complete checkbox select

--- managesite/views.py
+++ managesite/views.py
@@ -11,6 +11,9 @@ def set_index():
 	base_data['page_desc'] = u''
 	base_data['barcode_options'] = ['barcode_option1', 'barcode_option2', 'barcode_option3']
 	base_data['factory_options'] = ['factory_option1', 'factory_option2', 'factory_option3']
+	base_data['inner_code_factory_options'] = ['inner_code_factory_option1', 'inner_code_factory_option2', 'inner_code_factory_option3']
+	base_data['outer_code_factory_options'] = ['outer_code_factory_option1', 'outer_code_factory_option2', 'outer_code_factory_option3']
+	base_data['box_code_factory_options'] = ['box_code_factory_option1', 'box_code_factory_option2', 'box_code_factory_option3']
 	return base_data
 
 def index(request):
--- static/managesite/managesite.js
+++ static/managesite/managesite.js
@@ -2,8 +2,8 @@
  * Created by tk on 2016/11/26.
  */
 
-$("#newOrderModal").modal('show');
-$("#datePicker").datetimepicker({
+$("#new_order_modal").modal('show');
+$("#date_picker").datetimepicker({
     format: "yyyy年mm月dd日",
     showMeridian: true,
     todayBtn: true,
@@ -14,4 +14,27 @@ $("#datePicker").datetimepicker({
     todayHighlight: true
 });
 
+$("#new_order_modal").find(".check-factory").find("input").change(function () {
+    if($(this).is(":checked")) {
+        $(this).parent(".code_factory").removeClass("disabled-font");
+        $(this).parent(".code_factory").parent(".check-factory").find(".selectpicker").attr("disabled", false).selectpicker('refresh');
+    }
+    else {
+        $(this).parent(".code_factory").addClass("disabled-font");
+        $(this).parent(".code_factory").parent(".check-factory").find(".selectpicker").attr("disabled", true).selectpicker('refresh');
+    }
+});
 
+$("#make_order_btn").bind("click", function () {
+    $("#step1-body").hide(800);
+    $("#step2-body").removeClass("hidden");
+    $("#step2-foot").removeClass("hidden");
+    $("#step1-foot").addClass("hidden");
+});
+
+$("#back_btn").bind("click", function () {
+    $("#step2-body").addClass("hidden");
+    $("#step1-body").show();
+    $("#step1-foot").removeClass("hidden");
+    $("#step2-foot").addClass("hidden");
+});
