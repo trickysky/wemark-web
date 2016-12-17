f28dbc30e1a32913ad2eb56985c02fa1966da051
tiankun <iam@tiankun.me>
2016-12-17 15:34:35 +0800

    add batch value

--- managesite/views.py
+++ managesite/views.py
@@ -9,8 +9,16 @@ def set_index():
 	base_data['app_name'] = u'生产赋码'
 	base_data['page_name'] = u'百威啤酒'
 	base_data['page_desc'] = u''
-	base_data['barcode_options'] = ['barcode_option1', 'barcode_option2', 'barcode_option3']
-	base_data['factory_options'] = ['factory_option1', 'factory_option2', 'factory_option3']
+	base_data['barcode_options'] = [
+		{'text': 'barcode_option1', 'value': 'barcode_value_1'},
+		{'text': 'barcode_option2', 'value': 'barcode_value_2'},
+		{'text': 'barcode_option3', 'value': 'barcode_value_3'}
+	]
+	base_data['factory_options'] = [
+		{'text': 'factory_option1', 'value': 'factory_value_1'},
+		{'text': 'factory_option2', 'value': 'factory_value_2'},
+		{'text': 'factory_option3', 'value': 'factory_value_3'}
+	]
 	base_data['inner_code_factory_options'] = ['inner_code_factory_option1', 'inner_code_factory_option2', 'inner_code_factory_option3']
 	base_data['outer_code_factory_options'] = ['outer_code_factory_option1', 'outer_code_factory_option2', 'outer_code_factory_option3']
 	base_data['box_code_factory_options'] = ['box_code_factory_option1', 'box_code_factory_option2', 'box_code_factory_option3']
--- static/managesite/managesite.js
+++ static/managesite/managesite.js
@@ -24,14 +24,101 @@ $("#new_order_modal").find(".check-factory").find("input").change(function () {
     }
 });
 
+function refresh_total_count() {
+    var $box_count = $("#box_count");
+    var $bottle_count = $("#bottle_count");
+    var box_count = $box_count.val();
+    var bottle_count = $bottle_count.val();
+    if (!isNaN(box_count) && !isNaN(bottle_count)) {
+        $("#total_count").val(box_count * bottle_count);
+    }
+}
+
+$("#box_count").bind('input', refresh_total_count);
+$("#bottle_count").bind('input', refresh_total_count);
+
+$("#total_count_checkbox").change(function () {
+    var $box_count = $("#box_count");
+    var $bottle_count = $("#bottle_count");
+    if($(this).is(":checked")) {
+        $box_count.val(0);
+        $box_count.attr('disabled', true);
+        $bottle_count.val(0);
+        $bottle_count.attr('disabled', true);
+        $("#total_count").attr('disabled', false);
+    } else {
+        $box_count.attr('disabled', false);
+        $bottle_count.attr('disabled', false);
+        $("#total_count").attr('disabled', true);
+    }
+});
+
+
 $("#make_order_btn").bind("click", function () {
     $("#step1-body").hide(800);
     $("#step2-body").removeClass("hidden");
     $("#step2-foot").removeClass("hidden");
     $("#step1-foot").addClass("hidden");
+
+    // 时间
     var datetime = $("#date_picker").val();
     if (datetime) {
-        $("#batch_date").find(".batch-value").text(datetime);
+        var $batch_date_value = $("#batch_date").find(".batch-value");
+        $batch_date_value.text(datetime);
+        $batch_date_value.removeClass('red-font');
+    }
+
+    // 产品条码
+    var barcode = $("#barcode").find("option:selected").text();
+    var $batch_barcode_value = $("#batch_barcode").find(".batch-value");
+    if (barcode && barcode != '-') {
+        $batch_barcode_value.text(barcode);
+        $batch_barcode_value.removeClass('red-font');
+    }
+    else if (barcode == '-') {
+        $batch_barcode_value.text('未设置');
+        $batch_barcode_value.addClass('red-font');
+    }
+
+    // 激活工厂
+    var factory = $("#factory").find("option:selected").text();
+    var $batch_factory_value = $("#batch_factory").find(".batch-value");
+    if (factory && factory != '-') {
+        $batch_factory_value.text(factory);
+        $batch_factory_value.removeClass('red-font');
+    }
+    else if (factory == '-') {
+        $batch_factory_value.text('未设置');
+        $batch_factory_value.addClass('red-font');
+    }
+
+    // 生产数量
+    var $box_count = $("#box_count");
+    var $bottle_count = $("#bottle_count");
+    var $total_count = $("#total_count");
+    var box_count = $box_count.val();
+    var bottle_count = $bottle_count.val();
+    var total_count = $total_count.val();
+    var $batch_count_value = $("#batch_count").find(".batch-value");
+    if ($total_count.attr("disabled")) {
+        if (box_count>0 && bottle_count>0) {
+            $batch_count_value.text(box_count+"万箱"+" X "+bottle_count+"瓶/箱 = "+box_count*bottle_count+'万瓶');
+            $batch_count_value.removeClass('red-font');
+        }
+        else {
+            $batch_count_value.text("未设置");
+            $batch_count_value.addClass("red-font");
+        }
+    }
+    else {
+        if (total_count>0) {
+            $batch_count_value.text(total_count+'万瓶');
+            $batch_count_value.removeClass('red-font');
+        }
+        else {
+            $batch_count_value.text("未设置");
+            $batch_count_value.addClass("red-font");
+        }
     }
 });
 
