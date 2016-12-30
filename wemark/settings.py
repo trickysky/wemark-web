41a9a2e4888debbd61360d1b9c07b53bee19b808
tiankun <iam@tiankun.me>
2016-12-30 10:55:38 +0800

    add pg

--- wemark/settings.py
+++ wemark/settings.py
@@ -82,8 +82,20 @@ WSGI_APPLICATION = 'wemark.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wemark',
+        'USER': 'wemark',
+        'PASSWORD': 'wemark2016links',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
     }
 }
 
