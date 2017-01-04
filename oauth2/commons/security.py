c1935baeb1d66a30a78a518087d439df9470b443
hahahu <im@huhaoyu.com>
2017-01-04 09:11:41 +0800

    fixbug: update permissions if necessary

--- oauth2/commons/security.py
+++ oauth2/commons/security.py
@@ -27,14 +27,14 @@ class Subject(object):
         self.session = session
 
     def is_authenticated(self):
-        if self.validate_access_token() or self.refresh_token():
+        if self.__validate_access_token() or self.__refresh_token():
             return True
-        self.clear()
+        self.__clear()
         return False
 
     @staticmethod
     def redirect_to_authenticate(state=None):
-        builder = Subject.create_oauth2_builder(OAuth2Utils.SEGMENT_AUTHORIZE)
+        builder = Subject.__create_oauth2_builder(OAuth2Utils.SEGMENT_AUTHORIZE)
 
         builder.set_response_type(ResponseType.CODE).set_client_id(constants.CLIENT_ID) \
             .set_redirect_uri(constants.CLIENT_CALLBACK_URI).set_scope(Scope.WEB_LOGIN)
@@ -44,19 +44,19 @@ class Subject(object):
         return builder.build() if builder.validate() else None
 
     def authenticate(self, auth_code):
-        builder = Subject.create_oauth2_builder(OAuth2Utils.SEGMENT_ACCESS_TOKEN)
+        builder = Subject.__create_oauth2_builder(OAuth2Utils.SEGMENT_ACCESS_TOKEN)
 
         builder.set_client_id(constants.CLIENT_ID).set_client_secret(constants.CLIENT_SECRET).set_code(auth_code) \
             .set_grant_type(GrantType.AUTHORIZATION_CODE).set_redirect_uri(constants.CLIENT_CALLBACK_URI)
 
         if builder.validate():
             url = builder.build()
-            r = requests.post(url=url, headers=self.create_headers()).json()
+            r = requests.post(url=url, headers=self.__create_headers()).json()
             data = r.get(ResponseEntity.RET_DATA)
             if ResponseResult.is_ok(r.get(ResponseEntity.RET_ERROR_CODE)) and data:
                 self.__update_session_by_data(data=data)
                 self.get_user_info(force_update=True)
-                self.expire_all()
+                self.__expire_all()
                 return True
             else:
                 logging.error(r)
@@ -65,44 +65,44 @@ class Subject(object):
 
     def logout(self):
         refresh_token = self.get_session_by_key(Subject.SESSION_REFRESH_TOKEN)
-        self.clear()
+        self.__clear()
 
         if refresh_token:
-            builder = Subject.create_oauth2_builder(OAuth2Utils.SEGMENT_LOG_OFF)
+            builder = Subject.__create_oauth2_builder(OAuth2Utils.SEGMENT_LOG_OFF)
             builder.set_refresh_token(refresh_token).set_client_id(constants.CLIENT_ID)
             if builder.validate():
                 url = builder.build()
-                r = requests.post(url=url, headers=self.create_headers()).json()
+                r = requests.post(url=url, headers=self.__create_headers()).json()
                 return ResponseResult.is_ok(r.get(ResponseEntity.RET_ERROR_CODE))
 
         return False
 
-    def validate_access_token(self):
+    def __validate_access_token(self):
         if self.exists(Subject.SESSION_ACCESS_TOKEN) and self.exists(Subject.SESSION_OPEN_ID):
             builder = OAuth2RequestBuilder()
             builder.set_access_token(Subject.SESSION_ACCESS_TOKEN).set_client_id(constants.CLIENT_ID) \
                 .set_open_id(self.get_session_by_key(Subject.SESSION_OPEN_ID))
             if builder.validate():
                 url = builder.build()
-                r = requests.get(url=url, headers=self.create_headers()).json()
+                r = requests.get(url=url, headers=self.__create_headers()).json()
                 return ResponseResult.is_ok(r.get(ResponseEntity.RET_ERROR_CODE))
         return False
 
-    def refresh_token(self):
+    def __refresh_token(self):
         if not self.exists(Subject.SESSION_REFRESH_TOKEN):
             return False
 
         refresh_token = self.get_session_by_key(Subject.SESSION_REFRESH_TOKEN)
-        builder = self.create_oauth2_builder(OAuth2Utils.SEGMENT_REFRESH_TOKEN)
+        builder = self.__create_oauth2_builder(OAuth2Utils.SEGMENT_REFRESH_TOKEN)
         builder.set_client_id(constants.CLIENT_ID).set_client_secret(constants.CLIENT_SECRET).set_grant_type(
             GrantType.REFRESH_TOKEN).set_refresh_token(refresh_token)
         if builder.validate():
             url = builder.build()
-            r = requests.post(url=url, headers=self.create_headers()).json()
+            r = requests.post(url=url, headers=self.__create_headers()).json()
             data = r.get(ResponseEntity.RET_DATA)
             if ResponseResult.is_ok(r.get(ResponseEntity.RET_ERROR_CODE)) and data:
                 self.__update_session_by_data(data=data)
-                self.expire_all()
+                self.__expire_all()
                 return True
             else:
                 logging.error(r)
@@ -114,22 +114,19 @@ class Subject(object):
         if not force_update:
             return info_cached
 
-        builder = self.create_oauth2_builder(OAuth2Utils.SEGMENT_GET_USER_INFO)
+        builder = self.__create_oauth2_builder(OAuth2Utils.SEGMENT_GET_USER_INFO)
         open_id = self.get_session_by_key(Subject.SESSION_OPEN_ID)
         access_token = self.get_session_by_key(Subject.SESSION_ACCESS_TOKEN)
         builder.set_open_id(open_id).set_access_token(access_token).set_client_id(constants.CLIENT_ID)
         if builder.validate():
             url = builder.build()
-            r = requests.get(url=url, headers=self.create_headers()).json()
+            r = requests.get(url=url, headers=self.__create_headers()).json()
             data = r.get(ResponseEntity.RET_DATA)
             if ResponseResult.is_ok(r.get(ResponseEntity.RET_ERROR_CODE)) and data:
                 self.add_or_update_session_by_key(Subject.SESSION_USER_INFO, data)
                 return data
 
         return info_cached
-
-    def expire_all(self):
-        self.session.set_expiry(Subject.SESSION_EXPIRES_IN)
 
     def get_session(self):
         return self.session
@@ -138,21 +135,17 @@ class Subject(object):
         return self.exists(Subject.SESSION_ROLE) and self.get_session_by_key(Subject.SESSION_ROLE) == role_name
 
     def has_permission(self, permission_name):
-        if not self.exists(Subject.SESSION_PERMISSIONS):
+        if not self.exists(Subject.SESSION_ROLE):
             return False
 
-        permission_dict = self.get_session_by_key(Subject.SESSION_PERMISSIONS)
+        role_name = self.get_session_by_key(Subject.SESSION_ROLE)
+        permissions = AuthorizationService.get_permissions_by_role_name(role_name=role_name)
         resource, operation = AuthorizationService.parse_permission(permission_name)
         if resource and operation:
-            operations = permission_dict.get(resource)
+            operations = permissions.get(resource)
             return operation in operations
 
         return False
-
-    def clear(self):
-        for key in [Subject.SESSION_OPEN_ID, Subject.SESSION_USER_INFO, Subject.SESSION_ACCESS_TOKEN,
-                    Subject.SESSION_REFRESH_TOKEN, Subject.SESSION_ROLE, Subject.SESSION_PERMISSIONS]:
-            self.delete_session_by_key(key)
 
     def get_session_by_key(self, key):
         return self.session.get(key, default=None)
@@ -171,25 +164,31 @@ class Subject(object):
     def exists(self, key):
         return self.get_session_by_key(key) is not None
 
+    def __clear(self):
+        for key in [Subject.SESSION_OPEN_ID, Subject.SESSION_USER_INFO, Subject.SESSION_ACCESS_TOKEN,
+                    Subject.SESSION_REFRESH_TOKEN, Subject.SESSION_ROLE, Subject.SESSION_PERMISSIONS]:
+            self.delete_session_by_key(key)
+
+    def __expire_all(self):
+        self.session.set_expiry(Subject.SESSION_EXPIRES_IN)
+
     def __update_session_by_data(self, data):
         role_name = data.get(OAuth2Utils.ROLE)
-        permissions = AuthorizationService.get_permissions_by_role_name(role_name=role_name)
         session_kv = {
             Subject.SESSION_ACCESS_TOKEN: data.get(OAuth2Utils.ACCESS_TOKEN),
             Subject.SESSION_REFRESH_TOKEN: data.get(OAuth2Utils.REFRESH_TOKEN),
             Subject.SESSION_ROLE: role_name,
-            Subject.SESSION_PERMISSIONS: permissions,
             Subject.SESSION_OPEN_ID: data.get(OAuth2Utils.OPEN_ID),
         }
         self.add_or_update_session_by_dict(session_kv)
 
     @staticmethod
-    def create_oauth2_builder(segment):
+    def __create_oauth2_builder(segment):
         return OAuth2RequestBuilder().set_schema(constants.OAUTH2_SCHEMA).set_host(constants.OAUTH2_HOST) \
             .set_port(constants.OAUTH2_PORT).set_segment(segment)
 
     @staticmethod
-    def create_headers():
+    def __create_headers():
         return {
             'content-type': 'application/x-www-form-urlencoded',
         }
