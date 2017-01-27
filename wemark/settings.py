158d7efa09353d36303ece8c7c94a5e14f8cbf02
yuanmingyi <millan.yuan@gmail.com>
2017-01-27 13:27:50 +0800

    1. remove the middleware since unnecessary 2. change a href to fix the broken link

--- wemark/settings.py
+++ wemark/settings.py
@@ -53,8 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'wemark.middleware.AuthenticationMiddleware',
-    'wemark.middleware.MultipleProxyMiddleware',
+    'wemark.middleware.AuthenticationMiddleware'
 ]
 
 ROOT_URLCONF = 'wemark.urls'
